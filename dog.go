9e2011bb68b052630e4cb88fac79ea01721d8102
unknown <akshat.dhiman15@gmail.com>
2024-04-07 15:37:38 +0530

    add: go files

---
+++ dog.go
@@ -0,0 +1,7 @@
+package dog
+
+import "strings"
+
+func WhenGrownUp(s string) string {
+	return "When the puppy growa up it says: " + strings.ToUpper(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Akshat120/dog
+
+go 1.22.2
